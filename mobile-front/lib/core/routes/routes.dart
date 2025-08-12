ecb4bb9a639955afcde101156d0a6b6ce932d9ed
smallomg <139943397+smallomg@users.noreply.github.com>
2025-08-12 09:07:30 +0900

    페이지 루트 변경 및 서버 주소 변경

--- mobile-front/lib/core/routes/routes.dart
+++ mobile-front/lib/core/routes/routes.dart
@@ -2,4 +2,8 @@ class AppRoutes {
   static const String login = '/login';
   static const String home = '/home';
   static const String splash = '/splash';
+  static const String qnaCompose = '/qna/compose';
+  static const String qnaList = '/qna/list';
+  static const String faq = '/faq';
+  static const String guide = '/guide';
 }
