a98a686708829a58f5f6148ee4e500824dfb08d6
sohee <son5839@naver.com>
2025-08-13 11:16:56 +0900

    back) SpringSecurity 관련 의존성 추가, Access토큰에서 userId 얻는 로직 추가
    1. SpringSecurity 관련 의존성 추가
    2. Access토큰에서 userId 얻는 로직 추가(CurrentUid 어노테이션 추가)

---
+++ server/src/main/java/com/example/fund/common/CurrentUid.java
@@ -0,0 +1,13 @@
+package com.example.fund.common;
+
+import java.lang.annotation.*;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Target(ElementType.PARAMETER) // 메서드 파라미터에서만 사용
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal(expression = "claims['uid']") // 토큰에서 uid 클레임 추출
+public @interface CurrentUid {
+
+}
