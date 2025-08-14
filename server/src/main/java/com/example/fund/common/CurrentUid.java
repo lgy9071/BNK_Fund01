4453d425959ef0a5f264ce98ece34ce3c858dcf7
sohee <son5839@naver.com>
2025-08-14 14:07:57 +0900

    250813~0814 Back) 오류수정, application.properties, CurrentUid어노테이션추가 수정
    1. 오류수정
     - Refresh토큰 오류(자동로그인) 수정
     - Access토큰이 계속 만료되는 문제 수정(Decoder오류)
     - Access토큰이 만료되면 재발급 할수없는 문제 수정(로직문제)
    - CurrentUid 어노테이션 작동하지 않는 문제 수정

--- server/src/main/java/com/example/fund/common/CurrentUid.java
+++ server/src/main/java/com/example/fund/common/CurrentUid.java
@@ -4,10 +4,11 @@ import java.lang.annotation.*;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
-@Target(ElementType.PARAMETER) // 메서드 파라미터에서만 사용
+// @CurrentUid
+@Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@AuthenticationPrincipal(expression = "claims['uid']") // 토큰에서 uid 클레임 추출
+@AuthenticationPrincipal(expression = "getClaim('uid')" // ← 여기!
+)
 public @interface CurrentUid {
-    
 }
