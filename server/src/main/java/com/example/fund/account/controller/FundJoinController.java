cfa016fa2727c56768b3e950825cc20325db5727
ynzzi <syg7474@naver.com>
2025-08-19 14:29:53 +0900

    Controller 생성

--- server/src/main/java/com/example/fund/account/controller/FundJoinController.java
+++ server/src/main/java/com/example/fund/account/controller/FundJoinController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.fund.account.entity.Branch;
+
 @CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api")
@@ -11,4 +13,10 @@ public class FundJoinController {
 
 	// 펀드 가입
 	// 펀드상품 ID와 사용자 정보를 받아와서
+	
+	
+	// 지점 관리
+//	public Branch branch() {
+//		
+//	}
 }
