8c4e6fc9945caec6980dfa04e3741eb38a90741b
ynzzi <syg7474@naver.com>
2025-08-21 19:49:47 +0900

    fundStatusRepository 추가

--- server/src/main/java/com/example/fund/fund/repository_fund/FundStatusDailyRepository.java
+++ server/src/main/java/com/example/fund/fund/repository_fund/FundStatusDailyRepository.java
@@ -1,9 +1,12 @@
 package com.example.fund.fund.repository_fund;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.fund.fund.entity_fund.Fund;
@@ -14,5 +17,8 @@ public interface FundStatusDailyRepository extends JpaRepository<FundStatusDaily
 	
 	boolean existsByFundAndBaseDate(Fund fund, LocalDate baseDate);
 	
-	Optional<FundStatusDaily> findByFund_FundIdAndBaseDate(String fundId, LocalDate baseDate);
+	@Query("select d.navPrice from FundStatusDaily d " +
+	           "where d.fund.fundId = :fundId and d.baseDate = :baseDate")
+	    Optional<BigDecimal> findNavPriceByFundIdAndBaseDate(@Param("fundId") String fundId,
+	                                                         @Param("baseDate") LocalDate baseDate);
 }
